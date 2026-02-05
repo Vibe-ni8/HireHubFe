1a386f0ad3272f1d89fc357236b0a0e80a7d59bc
Vibe-ni8 <vigneshmalliga02@gmail.com>
2026-02-05 15:06:23 +0530

    Updated Add Candidate Page

--- src/pages/admin/candidateManagement/AddCandidate.tsx
+++ src/pages/admin/candidateManagement/AddCandidate.tsx
@@ -16,6 +16,7 @@ export default function AddCandidate() {
         previousCompany: null, experienceLevelName: '', techStack: [],
         resumeUrl: null, linkedInUrl: null, gitHubUrl: null
     });
+    const [newTech, setNewTech] = useState('');
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
       const { name, value } = e.target;
@@ -24,6 +25,28 @@ export default function AddCandidate() {
         [name]: value.toString()
       });
     };
+
+    const removeTech = (index: number) => {
+      let updatedTechStack = candidate.techStack.filter((_, idx) => idx !== index);
+      setCandidate({ ...candidate, techStack: updatedTechStack });
+    }
+
+    const addTech = () => {
+        if (!newTech.trim() || candidate.techStack.includes(newTech.trim())) {
+          setNewTech('');
+          return;
+        }
+        let updatedTechStack = [ ...candidate.techStack, newTech.trim() ];
+        setCandidate({ ...candidate, techStack: updatedTechStack });
+        setNewTech('');
+  }
+
+  const addTechInsteadSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') { 
+      e.preventDefault(); 
+      addTech();   
+    }
+  }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -53,25 +76,71 @@ export default function AddCandidate() {
             {/* Add User Section */}
             <form className="add-candidate-section" onSubmit={handleSubmit}>
                 <div className="ac-input-block">
-                    <label>Full Name:</label>
+                    <label>Full Name*:</label>
                     <input type="text" name="fullName" required value={candidate.fullName} onChange={handleChange} />
                 </div>
                 <div className="ac-input-block">
-                    <label>Email:</label>
+                    <label>Email*:</label>
                     <input type="text" name="email" required value={candidate.email} onChange={handleChange} placeholder="you@company.com" />
                 </div>
                 <div className="ac-input-block">
-                    <label>Phone:</label>
+                    <label>Phone*:</label>
                     <input type="number" name="phone" required value={candidate.phone} onChange={handleChange} placeholder="+91  _ _ _ _ _ _ _ _ _ _" />
                 </div>
                 <div className="ac-input-block">
-                    <label>Experience:</label>
+                    <label>Address:</label>
+                    <input type="text" name="address" value={candidate.address ?? ''} onChange={handleChange} />
+                </div>
+                <div className="ac-input-block">
+                    <label>College:</label>
+                    <input type="text" name="college" value={candidate.college ?? ''} onChange={handleChange} />
+                </div>
+                <div className="ac-input-block">
+                    <label>Previous Company:</label>
+                    <input type="text" name="previousCompany" value={candidate.previousCompany ?? ''} onChange={handleChange} />
+                </div>
+                <div className="ac-input-block">
+                    <label>Experience*:</label>
                     <select name="experienceLevelName" required value={candidate.experienceLevelName} onChange={handleChange} >
                         <option value=''>Select</option>
                         <option value='Fresher'>Fresher</option>
                         <option value='Intermediate'>Intermediate</option>
                         <option value='Experienced'>Experienced</option>
                     </select>
+                </div>
+                <div className="ac-input-block">
+                    <label>TeckStack:</label>
+                    <div className="ac-teckstack">
+                        <div className="cd-tags">
+                            {candidate?.techStack.map((tech, i) => (
+                            <span key={i} className="cd-tag">
+                                {tech}
+                                <button type="button" className="cd-remove" onClick={() => removeTech(i)} > 
+                                    × 
+                                </button>
+                            </span>
+                            ))}
+                        </div>
+                        <div className="cd-add">
+                            <input type="text" placeholder="Add tech" value={newTech} 
+                                onChange={e => setNewTech(e.target.value)} 
+                                onKeyDown={addTechInsteadSubmit} 
+                            />
+                            <button type="button" onClick={addTech}> Add </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="ac-input-block">
+                    <label>Resume url:</label>
+                    <input type="text" name="resumeUrl" value={candidate.resumeUrl ?? ''} onChange={handleChange} />
+                </div>
+                <div className="ac-input-block">
+                    <label>LinkedIn url:</label>
+                    <input type="text" name="linkedInUrl" value={candidate.linkedInUrl ?? ''} onChange={handleChange} />
+                </div>
+                <div className="ac-input-block">
+                    <label>GitHub url:</label>
+                    <input type="text" name="gitHubUrl" value={candidate.gitHubUrl ?? ''} onChange={handleChange} />
                 </div>
                 <div className="ac-button-block">
                     <button type="submit">Add</button>
